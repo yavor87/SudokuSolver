4b42c65969fb1f89b1b8f58237ccb05b63e616c0
Yavor <yavor87@gmail.com>
2013-11-14 23:21:02 +0200

    Added tests for Cell

--- src/sudokuSolver/board/Cell.java
+++ src/sudokuSolver/board/Cell.java
@@ -9,7 +9,6 @@ public class Cell {
 	public static final int Empty = 0;
 	private int value;
 	private ArrayList<Integer> valuePossibilities;
-
 	private Block block;
 
 	public Cell() {
@@ -31,8 +30,19 @@ public class Cell {
 	 * @param value the value to set
 	 */
 	public void setValue(int value) {
+		if (value < 0 || value > 9) {
+			throw new IllegalArgumentException("the value of the Cell should be between 0 and 9");
+		}
+		
 		this.value = value;
 		this.onValueChanged();
+	}
+	
+	/**
+	 * Sets the value of this cell to {@link Cell.Empty}.
+	 */
+	public void clearValue() {
+		this.value = Cell.Empty;
 	}
 	
 	/**
@@ -54,16 +64,28 @@ public class Cell {
 	/**
 	 * Adds a value possibility for this cell.
 	 * @param valuePossibility The value possibility to add.
+	 * @throws IllegalArgumentException
 	 */
 	public void addValuePossibility(int valuePossibility) {
-		this.valuePossibilities.add(valuePossibility);
+		if (valuePossibility < 1 || valuePossibility > 9) {
+			throw new IllegalArgumentException("the value posibility should be between 1 and 9");
+		}
+			
+		if (!this.valuePossibilities.contains(valuePossibility)) {
+			this.valuePossibilities.add(valuePossibility);
+		}
 	}
 	
 	/**
 	 * Removes a value possibility for this cell.
 	 * @param valuePossibility The value possibility to remove.
+	 * @throws IllegalArgumentException
 	 */
 	public void removeValuePossibility(int valuePossibility) {
+		if (valuePossibility < 1 || valuePossibility > 9) {
+			throw new IllegalArgumentException("the value posibility should be between 1 and 9");
+		}
+		
 		this.valuePossibilities.remove((Object)valuePossibility);
 	}
 	
--- test/sudokuSolver/board/CellTest.java
+++ test/sudokuSolver/board/CellTest.java
@@ -1,39 +1,107 @@
 package sudokuSolver.board;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CellTest {
-
+	private Cell cell;
+	
+	@Before
+	public void testInitialize() {
+		this.cell = new Cell();
+	}
+	
+	@Test
+	public void testCellCreate() {
+		assertEquals(Cell.Empty, this.cell.getValue());
+		ArrayList<Integer> valuePossibilites = this.cell.getValuePossibilities();
+		assertNotNull(valuePossibilites);
+		assertEquals(9, valuePossibilites.size());
+		for (int i = 0; i < valuePossibilites.size(); i++) {
+			int valuePossibility = valuePossibilites.get(i);
+			assertEquals(i + 1, valuePossibility);
+		}
+	}
+	
 	@Test
 	public void testGetValue() {
-		fail("Not yet implemented");
+		int cellValue = this.cell.getValue();
+		assertEquals(Cell.Empty, cellValue);
 	}
 
 	@Test
 	public void testSetValue() {
-		fail("Not yet implemented");
+		for (int i = 0; i < 10; i++) {
+			int target = i;
+			this.cell.setValue(target);
+			assertEquals(target, this.cell.getValue());	
+		}
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetInvalidValue() {
+		this.cell.setValue(10);
+	}
+	
+	@Test
+	public void testSetEmptyValue() {
+		this.cell.setValue(3);
+		this.cell.setValue(Cell.Empty);
+		assertEquals(Cell.Empty, this.cell.getValue());
+	}
+	
+	@Test
+	public void testClearValue() {
+		this.cell.setValue(3);
+		this.cell.clearValue();
+		assertEquals(Cell.Empty, this.cell.getValue());
 	}
 
 	@Test
 	public void testGetBlock() {
-		fail("Not yet implemented");
+		assertNull(this.cell.getBlock());
 	}
 
 	@Test
 	public void testGetValuePossibilities() {
-		fail("Not yet implemented");
+		assertNotNull(this.cell.getValuePossibilities());
 	}
 
 	@Test
 	public void testAddValuePossibility() {
-		fail("Not yet implemented");
+		for (int i = 1; i < 10; i++) {
+			this.cell.addValuePossibility(i);
+			assertEquals(9, this.cell.getValuePossibilities().size());
+		}
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddEmptyValuePossibility() {
+		this.cell.addValuePossibility(Cell.Empty);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddInvalidValuePossibility() {
+		this.cell.addValuePossibility(10);
 	}
 
 	@Test
 	public void testRemoveValuePossibility() {
-		fail("Not yet implemented");
+		for (int i = 1; i < 10; i++) {
+			this.cell.removeValuePossibility(i);
+			assertEquals(9 - i, this.cell.getValuePossibilities().size());
+		}
 	}
 
+	@Test
+	public void testRemoveValuePossibilityTwice() {
+		this.cell.removeValuePossibility(3);
+		this.cell.removeValuePossibility(3);
+	}
 }
